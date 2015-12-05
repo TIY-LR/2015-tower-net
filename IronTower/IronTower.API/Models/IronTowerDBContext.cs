656a3b5127a4afc6c0ec1a15500c65c29287dd68
Justin Wyatt <justin@wyattcorporation.com>
2015-12-04 23:24:08 -0600

    fixed cs name reference

--- IronTower/IronTower.API/Models/IronTowerDBContext.cs
+++ IronTower/IronTower.API/Models/IronTowerDBContext.cs
@@ -14,7 +14,7 @@ namespace IronTower.API.Models
         // If you wish to target a different database and/or database provider, modify the 'IronTowerDBContext' 
         // connection string in the application configuration file.
         public IronTowerDBContext()
-            : base("name=IronTowerDBContext")
+            : base("name=IronTowerDB")
         {
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<IronTowerDBContext, Configuration>());
         }
